d98a7ce7032fe9ec6ce46a77d21d7753929e4399
Tash <tlee0160@student.monash.edu>
2024-12-01 16:26:05 +1100

    import data test - typescript

---
+++ AOC24Day1.js
@@ -0,0 +1,6 @@
+// 'Distance between two lists':
+// Pair up entries from each list by matching the places when arranged according to numerical value
+// Find the 'distance' (difference) between each entry pair and add these up for all entry pairs
+
+import listsData from './adventOfCode2024Day1-puzzleInput';
+console.log(listsData);
