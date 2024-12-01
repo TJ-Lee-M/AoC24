945959423b05d4f004f4fa516e4b1a48caf7b3d2
Tash <tlee0160@student.monash.edu>
2024-12-01 16:35:22 +1100

    installing typecript

--- AOC24Day1.js
+++ AOC24Day1.js
@@ -2,5 +2,7 @@
 // Pair up entries from each list by matching the places when arranged according to numerical value
 // Find the 'distance' (difference) between each entry pair and add these up for all entry pairs
 
+npm install -D typescript //globally install typecript
+
 import listsData from './adventOfCode2024Day1-puzzleInput';
-console.log(listsData);
+console.log(listsData); // print imprted data
